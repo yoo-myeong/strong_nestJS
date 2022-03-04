dac7d2ecf2a86b877567155b325cb0484ae3ad22
yoo-myeong <yoou201@naver.com>
2022-03-04 21:43:51 +0900

    mvc패턴 랜덤채팅 프로젝트 셋팅

---
+++ randomChat/chattings/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  private readonly idDev: boolean = process.env.MODE === 'dev' ? true : false;
+
+  configure() {
+    mongoose.set('debug', this.idDev);
+  }
+}
